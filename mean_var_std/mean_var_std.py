44ef2c3fca84c874c466d1c16b803427e696ceb5
SauryANever23 <sauryajha9841@gmail.com>
2025-06-11 13:32:57 +0545

    adding the function calculate() that takes a list as input and gives out a dictionary as output

--- mean_var_std/mean_var_std.py
+++ mean_var_std/mean_var_std.py
@@ -1,1 +1,14 @@
+import numpy as np 
 
+def calculate(list):
+    """
+    take a list of 9 elements and converts them into a 3 x 3 matrix, 
+    then gives out the mean, variance, std, max, min, sum of all the coloums(axis=0) and
+    the same for rows(axis=1) and gives a flat value of the entire list. 
+    """
+    
+    if len(list) != 9:
+        raise ValueError("List should have 9 elements")
+    
+    
+    return calculations
