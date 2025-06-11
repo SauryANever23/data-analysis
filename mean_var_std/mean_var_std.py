df08ebcf8351e5d0f300d35a3329ce5f95eb7291
SauryANever23 <sauryajha9841@gmail.com>
2025-06-11 13:36:21 +0545

    adding the list values in the dict calculations

--- mean_var_std/mean_var_std.py
+++ mean_var_std/mean_var_std.py
@@ -1,6 +1,6 @@
 import numpy as np 
 
-def calculate(list):
+def calculate(list: list) -> dict:
     """
     take a list of 9 elements and converts them into a 3 x 3 matrix, 
     then gives out the mean, variance, std, max, min, sum of all the coloums(axis=0) and
@@ -9,6 +9,27 @@ def calculate(list):
     
     if len(list) != 9:
         raise ValueError("List should have 9 elements")
+
+    arr = np.array(list)
+
+    matrix = arr.reshape(3, 3)
+
+    calculations = {
+        'mean': [
+            matrix.mean(axis=0).tolist(),
+            matrix.mean(axis=1).tolist(),
+            matrix.mean().tolist()
+        ],
+        'var': [
+            matrix.var(axis=0).tolist(),
+            matrix.var(axis=1).tolist(),
+            matrix.var().tolist()
+        ],
+        'standard deviation': [
+            matrix.std(axis=0, dtype("float16"), ddof=0).tolist(),
+            
+        ]
+    }
     
     
     return calculations
